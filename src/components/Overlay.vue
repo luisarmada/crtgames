2f6b3194df02e960c4ddd6d5e532c3e9568889da
Luis Armada <Luis.Armada@warwick.ac.uk>
2024-05-28 23:48:34 +0100

    screen responsiveness

---
+++ src/components/Overlay.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+<body>
+  <div id="monitor">
+    
+    <div id="screenspace">
+
+    </div>
+    <img src="../assets/jcpennybedroom_scratched.png" />
+  </div>
+</body>
+</template>
+
+<style lang="css">
+body {
+  overflow: hidden;
+}
+#monitor{
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+}
+
+#monitor img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  pointer-events: none;
+  user-select: none;
+  object-position: bottom;
+  z-index: 11;
+}
+
+#screenspace {
+  position: absolute;
+  right: 25vw;
+  height: 37vw;
+  bottom: 9.5vw;
+  left: 25vw;
+  z-index: 10;
+  background-color: green;
+}
+
+
+
+@media only screen and (max-width: 178vh ) {
+  #screenspace{
+    background-color: blue;
+    bottom: calc(100%  - 83%);
+    left: 0;
+    right: 0;
+    height: 66vh;
+  }
+}
+@media only screen and (max-width: 89vh ) {
+  #screenspace{
+    background-color: red;
+    bottom: calc(100%  - 83%);
+    left: 0vw;
+    right: 0vw;
+    height: 66vh;
+  }
+}
+</style>
