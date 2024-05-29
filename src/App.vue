92382b549b03675a4a43700c927698542f70d4ed
Luis Armada <Luis.Armada@warwick.ac.uk>
2024-05-29 01:17:26 +0100

    crt effect

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,69 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import Overlay from './components/Overlay.vue'
+import CRTOverlay from './components/CRTOverlay.vue'
 </script>
 
 <template>
-  <header>
-    <Overlay />
-  </header>
+  <body>
+    <div id="monitor">
+      
+      <div id="screenspace">
+        <CRTOverlay />
+      </div>
+      <img src="./assets/c1902bedroom_glare.png" />
+    </div>
+  </body>
 </template>
 
 <style scoped>
+  body {
+    overflow: hidden;
+  }
+#monitor{
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+}
+
+#monitor img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  pointer-events: none;
+  user-select: none;
+  object-position: bottom;
+  z-index: 11;
+}
+
+#screenspace {
+  position: absolute;
+  right: 25vw;
+  height: 37vw;
+  bottom: 9.5vw;
+  left: 25vw;
+  z-index: 10;
+  background-color: green;
+}
+
+@media only screen and (max-width: 178vh ) {
+  #screenspace{
+    /*background-color: blue;*/
+    bottom: calc(100%  - 83%);
+    left: 0;
+    right: 0;
+    height: 66vh;
+  }
+}
+@media only screen and (max-width: 89vh ) {
+  #screenspace{
+    /*background-color: red;*/
+    bottom: calc(100%  - 83%);
+    left: 0vw;
+    right: 0vw;
+    height: 66vh;
+  }
+}
 
 </style>
---
+++ src/components/ScreenContents.vue
@@ -0,0 +1,65 @@
+<script>
+
+</script>
+
+<template>
+    <body>
+    <div id="wrapper">
+      <h1 id="myname">Luis Armada</h1>
+      <h2 id="subtitle">a portfolio.</h2>
+    </div>
+  </body>
+</template>
+
+<style>
+ @font-face {
+    font-family: 'Noto Sans Display';
+    font-style: normal;
+    font-weight: 400;
+    src: url(../assets/NotoSansDisplay-Regular.ttf);
+  }
+  body {
+    font-family: 'Noto Sans Display';
+    
+    background: #000;
+    color: #eee;
+
+    text-shadow: 0.06rem 0 0.06rem #ea36af, -0.125rem 0 0.06rem #75fa69;
+    animation-duration: 0.015s;
+    animation-name: textflicker;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+
+    overflow: hidden;
+  }
+  #wrapper {
+    width: 23vw;
+    left: 50%;
+    transform: translate(-50%, 0);
+    position: absolute;
+    top: 35%;
+    height: 100vh;
+    text-align: center;
+  }
+
+  #myname {
+    font-size: 4vw;
+    letter-spacing: -0.0125em;
+    font-weight: 800;
+  }
+
+  #subtitle {
+    font-size: 1vw;
+    text-align: right;
+    margin-top: -30px;
+  }
+
+  @keyframes textflicker {
+    from {
+      text-shadow: 1px 0 0 #ea36af, -2px 0 0 #75fa69;
+    }
+    to {
+      text-shadow: 2px 0.5px 2px #ea36af, -1px -0.5px 2px #75fa69;
+    }
+  }
+</style>
